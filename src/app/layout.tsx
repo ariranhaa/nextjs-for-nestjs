4c1c2e40faec9909f8af52b59c153ff3be56ff90
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-05-21 17:06:07 -0300

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Blog com next.js",
+  description: "Descrição da pagina",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body>
+        <header>Header</header>
+        {children}
+        <footer>Footer</footer>
+      </body>
+    </html>
+  );
+}
