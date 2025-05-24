ab5279a229b50f8d737f96aa5a11edcec4a43128
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-05-23 21:37:48 -0300

    night night

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import { Header } from "@/components/Header";
+import { PostsList } from "@/components/PostsList";
+import { SpinLoader } from "@/components/SpinLoader";
+import { Suspense } from "react";
 
 // page.tsx(server) -> menu.tsx(server) -> link(client)
 
 export default async function HomePage() {
-  console.log("Home Page");
   return (
     <div>
-      <Header />
+      <header className="text-6xl font-bold text-center py-8">
+        Aqui é a header
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer className="text-6xl font-bold text-center py-8">Footer</footer>
     </div>
   );
 }
--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,8 +11,15 @@ const JSON_POSTS_FILE_PATH = resolve(
   "seed",
   "posts.json"
 );
+const SIMULATE_WAIT_IN_MS = 5000;
 
 export class JsonPostRepository implements PostRepository {
+  private async simulateWait() {
+    if (SIMULATE_WAIT_IN_MS <= 0) return;
+
+    await new Promise((resolve) => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk(): Promise<PostModel[]> {
     const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
     const parsedJson = JSON.parse(jsonContent);
@@ -20,10 +27,14 @@ export class JsonPostRepository implements PostRepository {
     return posts;
   }
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
   async findById(id: string): Promise<PostModel> {
+    await this.simulateWait();
+
     const posts = await this.findAll();
     const post = posts.find((post) => post.id === id);
 
@@ -32,5 +43,3 @@ export class JsonPostRepository implements PostRepository {
     return post;
   }
 }
-
-export const postRepository: PostRepository = new JsonPostRepository();
