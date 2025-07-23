7881b5987d4edbc11f83a3c33b0827ca4c662bd7
Arthur Sottoriva <arthursottoriva49@gmail.com>
2025-07-23 12:33:20 -0300

    2307

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -3,12 +3,20 @@
 import { Button } from "@/components/Button";
 import { InputCheckbox } from "@/components/InputCheckbox";
 import { InputText } from "@/components/InputText";
+import { MarkdownEditor } from "@/components/MarkdownEditor";
+import { useState } from "react";
+import { ImageUploader } from "../ImageUploader";
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState("");
+
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
         <InputText labelText="Nome" placeholder="Digite seu nome" />
+
+        <ImageUploader />
+
         <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
         <InputCheckbox labelText="Sobrenome" />
         <InputText
@@ -17,6 +25,15 @@ export function ManagePostForm() {
           placeholder="Digite seu nome"
           defaultValue="Hello"
         />
+
+        <MarkdownEditor
+          labelText="Conteúdo"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue}
+        />
+
         <InputText
           disabled
           labelText="Sobrenome"
